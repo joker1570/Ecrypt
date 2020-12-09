15d0b31b19e93d68efeeb866c564d3ce9276edf0
xiao_wugui <1325412974@qq.com>
2020-12-09 17:27:05 +0800

    SecKeyClient

---
+++ SecKeyServer/SecKeyServer/ShareMemory.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include "ShareMemory.h"
+using namespace std;
+
+ShareMemory::ShareMemory(int key)
+{
+	m_shmID = shmget(key, 0, 0);
+}
+
+ShareMemory::ShareMemory(int key, int size)
+{
+	m_shmID = shmget(key, size, IPC_CREAT | 0766);	
+}
+
+ShareMemory::ShareMemory(const char* name)
+{
+	key_t key = ftok(name, RandX);
+	m_shmID = shmget(key, 0, 0);
+}
+
+ShareMemory::ShareMemory(const char* name, int size)
+{
+	key_t key = ftok(name, RandX);
+	m_shmID = shmget(key, size, IPC_CREAT | 0766);
+}
+
+ShareMemory::~ShareMemory()
+{
+	if(m_shmAddr!=NULL)
+		unmapShm();
+}
+
+void *ShareMemory::mapShm()
+{
+	m_shmAddr = shmat(m_shmID, NULL, 0);
+	if(m_shmAddr==(void *)-1)
+	{
+		return NULL;
+	}
+	
+	return m_shmAddr;
+}
+
+int ShareMemory::unmapShm()
+{
+	int ret = shmdt(m_shmAddr);
+	m_shmAddr=NULL;
+	return ret;
+}
+
+int ShareMemory::delShm()
+{
+	int ret = shmctl(m_shmID, IPC_RMID, NULL);
+	
+	return ret;
+}
+
+
+
+
